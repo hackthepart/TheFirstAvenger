e7432add5087d845aa698cf2f29361298ee14ad7
iOSDev747 <iOSDev747@iosdev747>
2017-12-17 00:08:31 +0530

    game made multi-player - initial commit

--- TicTacToe/src/tictactoe/GameWindow.java
+++ TicTacToe/src/tictactoe/GameWindow.java
@@ -12,6 +12,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -26,6 +33,14 @@ public class GameWindow extends javax.swing.JFrame {
     private JLabel playingLabels[][] = new JLabel[3][3];
     private final int PLAYINGAREAX = 110, PLAYINGAREAY = 100, CELLSIZE = 40, SPACEBETWEENCELLS = 10;
     private int currentPlayer = 0;
+    private boolean isServerBoolean = true;
+    private final int port = 747;
+    private String playerName,opponentName;
+    JPanel playingArea = new JPanel();
+    ServerSocket serverSocket;
+    Socket _serverSocket,_clientSocket;
+    Scanner serverGetNameOfClient,clientGetNameOfServer;
+    PrintStream clientSendName,serverSendName;
     
     private void reset(){
         for(int i = 0; i < 3; i++){
@@ -130,10 +145,9 @@ public class GameWindow extends javax.swing.JFrame {
         initComponents();
         
         // Adding panel
-        JPanel playingArea = new JPanel();
         playingArea.setLocation(PLAYINGAREAX, PLAYINGAREAY);
         playingArea.setSize( 3 * (CELLSIZE + SPACEBETWEENCELLS) , 3 * (CELLSIZE + SPACEBETWEENCELLS));
-        playingArea.setVisible(true);
+        playingArea.setVisible(false);  //not visible initially
         playingArea.setLayout(null);
         add(playingArea);
         addPlayingComponents(playingArea);
@@ -149,33 +163,198 @@ public class GameWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup = new javax.swing.ButtonGroup();
         title = new javax.swing.JLabel();
+        isServer = new javax.swing.JRadioButton();
+        isClient = new javax.swing.JRadioButton();
+        nameLabel = new javax.swing.JLabel();
+        nameField = new javax.swing.JTextField();
+        symbolLabel = new javax.swing.JLabel();
+        symbolField = new javax.swing.JTextField();
+        startButton = new javax.swing.JButton();
+        ipField = new javax.swing.JTextField();
+        ipLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         title.setFont(new java.awt.Font("Ubuntu", 1, 24)); // NOI18N
         title.setText("TIC TAC TOE");
 
+        buttonGroup.add(isServer);
+        isServer.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        isServer.setSelected(true);
+        isServer.setText("Server");
+        isServer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                isServerActionPerformed(evt);
+            }
+        });
+
+        buttonGroup.add(isClient);
+        isClient.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        isClient.setText("Client ");
+        isClient.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                isClientActionPerformed(evt);
+            }
+        });
+
+        nameLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        nameLabel.setText("Name");
+
+        symbolLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        symbolLabel.setText("Symbol");
+
+        startButton.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        startButton.setForeground(new java.awt.Color(0, 200, 100));
+        startButton.setText("Start");
+        startButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startButtonActionPerformed(evt);
+            }
+        });
+
+        ipField.setEnabled(false);
+
+        ipLabel.setText("IP:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(119, 119, 119)
-                .addComponent(title)
-                .addContainerGap(108, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(119, 119, 119)
+                        .addComponent(title)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(symbolLabel)
+                        .addGap(18, 18, 18)
+                        .addComponent(symbolField, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(isClient, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(isServer, javax.swing.GroupLayout.Alignment.TRAILING))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(ipLabel)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(ipField, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(startButton, javax.swing.GroupLayout.DEFAULT_SIZE, 164, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(nameLabel)
+                                .addGap(18, 18, 18)
+                                .addComponent(nameField)))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(title)
-                .addContainerGap(323, Short.MAX_VALUE))
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nameLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(symbolField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(symbolLabel))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 163, Short.MAX_VALUE)
+                        .addComponent(isServer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(isClient)
+                            .addComponent(ipField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(ipLabel)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(startButton)))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startButtonActionPerformed
+        // TODO add your handling code here:
+        if(nameField.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Enter Name");
+        }
+        else{
+            if(symbolField.getText().equals("")){
+                JOptionPane.showMessageDialog(null, "Enter Symbol");
+            }
+            else{
+                if(isClient.isSelected()){
+                    if(ipField.getText().equals("")){
+                       JOptionPane.showMessageDialog(null, "Enter IP");
+                    }
+                    else{
+                       assign();
+                        try {
+                            _clientSocket = new Socket(ipField.getText(),port);
+                            clientSendName = new PrintStream(_clientSocket.getOutputStream());
+                            clientSendName.println(nameField.getText());
+                            clientGetNameOfServer = new Scanner(_clientSocket.getInputStream());
+                            opponentName = clientGetNameOfServer.nextLine();
+                            
+                        } catch (IOException ex) {
+                            Logger.getLogger(GameWindow.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+                else{
+                    assign();
+                    try {
+                        serverSocket = new ServerSocket(port);
+                        _serverSocket = serverSocket.accept();
+                        serverGetNameOfClient = new Scanner(_serverSocket.getInputStream());
+                        opponentName = serverGetNameOfClient.nextLine();
+                        serverSendName = new PrintStream(_serverSocket.getOutputStream());
+                        serverSendName.println(nameField.getText());
+                    } catch (IOException ex) {
+                        Logger.getLogger(GameWindow.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+    }//GEN-LAST:event_startButtonActionPerformed
+
+    private void isServerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_isServerActionPerformed
+        // TODO add your handling code here:
+        isServerBoolean = true;
+        ipField.setEnabled(false);
+    }//GEN-LAST:event_isServerActionPerformed
+
+    private void isClientActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_isClientActionPerformed
+        // TODO add your handling code here:
+        isServerBoolean = false;
+        ipField.setEnabled(true);
+    }//GEN-LAST:event_isClientActionPerformed
+
+    public void assign(){
+        /*assign names . . .. . to string  now*/
+        playerName = nameField.getText();
+        playingArea.setVisible(true);
+        isServer.setVisible(false);
+        isClient.setVisible(false);
+        ipLabel.setVisible(false);
+        ipField.setVisible(false);
+        startButton.setVisible(false);
+        symbolLabel.setVisible(false);
+        nameLabel.setVisible(false);
+        symbolField.setVisible(false);
+        nameField.setVisible(false);
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -212,6 +391,16 @@ public class GameWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup;
+    private javax.swing.JTextField ipField;
+    private javax.swing.JLabel ipLabel;
+    private javax.swing.JRadioButton isClient;
+    private javax.swing.JRadioButton isServer;
+    private javax.swing.JTextField nameField;
+    private javax.swing.JLabel nameLabel;
+    private javax.swing.JButton startButton;
+    private javax.swing.JTextField symbolField;
+    private javax.swing.JLabel symbolLabel;
     private javax.swing.JLabel title;
     // End of variables declaration//GEN-END:variables
 }
