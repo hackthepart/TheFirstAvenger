e6d82e4d1f63a68c68a2bebed734c883988e8b9a
iosdev474 <iosdev474@iosdev474-pc>
2017-12-17 01:22:48 -0800

    multiplayer added upto names and symbol

--- TicTacToe/src/tictactoe/GameWindow.java
+++ TicTacToe/src/tictactoe/GameWindow.java
@@ -12,7 +12,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,8 +32,16 @@ public class GameWindow extends javax.swing.JFrame {
      * Creates new form GameWindow
      */
     private JLabel playingLabels[][] = new JLabel[3][3];
-    private final int PLAYINGAREAX = 110, PLAYINGAREAY = 100, CELLSIZE = 40, SPACEBETWEENCELLS = 10;
-    private int currentPlayer = 0;
+    private final int PLAYINGAREAX = 150, PLAYINGAREAY = 100, CELLSIZE = 40, SPACEBETWEENCELLS = 10;
+    private int currentPlayer = 0,port = 474;
+    boolean playing=false,connected=false;
+    String ip = "127.0.0.1";
+    String player,opponent;
+    String playerSymbol,opponentSymbol;
+    Socket cSocket,sSocket;
+    ServerSocket serSocket;
+    Scanner cScanner,sScanner;
+    PrintStream cPrintStream,sPrintStream;
     
     private void reset(){
         for(int i = 0; i < 3; i++){
@@ -95,15 +111,26 @@ public class GameWindow extends javax.swing.JFrame {
     }
     
     private void cellClicked(MouseEvent evt){
-        JLabel currentCell = (JLabel) evt.getComponent();
-        if(currentPlayer == 0){
-            currentCell.setText("O");
+        if(!connected)
+        {
+            JOptionPane.showMessageDialog(null, "Wait for opponent to connect.");
         }
-        else{
-            currentCell.setText("X");
+        else if(connected&&!playing)
+        {
+            JOptionPane.showMessageDialog(null, "Wait for opponent turn");
         }
-        currentPlayer = (currentPlayer + 1) % 2;
-        check( (currentPlayer + 1) % 2 );
+        else
+        {
+            JLabel currentCell = (JLabel) evt.getComponent();
+            if(currentPlayer == 0){
+                currentCell.setText("O");
+            }
+            else{
+                currentCell.setText("X");
+            }
+            currentPlayer = (currentPlayer + 1) % 2;
+            check( (currentPlayer + 1) % 2 );
+        }
     }
     
     private void addPlayingComponents(JPanel playingArea){
@@ -137,7 +164,7 @@ public class GameWindow extends javax.swing.JFrame {
         playingArea.setLayout(null);
         add(playingArea);
         addPlayingComponents(playingArea);
-        
+    
     }
 
     /**
@@ -150,31 +177,181 @@ public class GameWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         title = new javax.swing.JLabel();
+        cli_ser_ComboBox = new javax.swing.JComboBox<>();
+        nameTextField = new javax.swing.JTextField();
+        symbolTextField = new javax.swing.JTextField();
+        actionButton = new javax.swing.JButton();
+        ipTextField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         title.setFont(new java.awt.Font("Ubuntu", 1, 24)); // NOI18N
         title.setText("TIC TAC TOE");
 
+        cli_ser_ComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Server", "Client" }));
+        cli_ser_ComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cli_ser_ComboBoxActionPerformed(evt);
+            }
+        });
+
+        nameTextField.setText("Your Name");
+
+        symbolTextField.setText("O");
+
+        actionButton.setText("Host");
+        actionButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                actionButtonActionPerformed(evt);
+            }
+        });
+
+        ipTextField.setText("IP Address");
+        ipTextField.setEnabled(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(119, 119, 119)
-                .addComponent(title)
-                .addContainerGap(108, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(title)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(cli_ser_ComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(nameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(ipTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(symbolTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(actionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(title)
-                .addContainerGap(323, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cli_ser_ComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(symbolTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(actionButton)
+                    .addComponent(ipTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(294, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void actionButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_actionButtonActionPerformed
+        actionButton.setEnabled(false);
+        if(cli_ser_ComboBox.getSelectedItem().equals("Server"))
+        {
+            if(nameTextField.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "Please enter name.");                
+            }
+            else if(symbolTextField.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "Please enter symbol.");                
+            }
+            else
+            {
+                try 
+                {
+                    player=nameTextField.getText();
+                    playerSymbol=symbolTextField.getText();
+                    serSocket = new ServerSocket(port);
+                    sSocket = serSocket.accept();
+                    sScanner = new Scanner(sSocket.getInputStream());
+                    opponent=sScanner.nextLine();
+                    sPrintStream.println(player);
+                    opponentSymbol=sScanner.nextLine();
+                    sPrintStream.println(playerSymbol);
+                    playing=true;
+                    connected=true;
+                } 
+                catch (IOException ex) 
+                {
+                    Logger.getLogger(GameWindow.class.getName()).log(Level.SEVERE, null, ex);
+                    actionButton.setEnabled(true);
+                }
+            }
+        }
+        else
+        {
+            if(ipTextField.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "Please enter IP Address.");
+            }
+            else if(nameTextField.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "Please enter name.");                
+            }
+            else if(symbolTextField.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "Please enter symbol.");                
+            }
+            else
+            {
+                try 
+                {
+                    ip=ipTextField.getText();
+                    player=nameTextField.getText();
+                    playerSymbol=symbolTextField.getText();
+                    cSocket = new Socket(ip,port);
+                    cPrintStream = new PrintStream(cSocket.getOutputStream());
+                    cScanner = new Scanner(cSocket.getInputStream());
+                    cPrintStream.println(player);
+                    opponent=cScanner.nextLine();
+                    cPrintStream.println(playerSymbol);
+                    opponentSymbol=cScanner.nextLine();
+                    if(opponentSymbol.equals(playerSymbol))
+                    {
+                        JOptionPane.showMessageDialog(null, "Same symbol: Changing to default");
+                        if(opponentSymbol.equals("O"))
+                        {
+                            playerSymbol="X";
+                        }
+                        else
+                        {
+                            playerSymbol="O";
+                        }
+                        symbolTextField.setText(playerSymbol);
+                    }
+                    playing=true;
+                    connected=true;
+                } 
+                catch (IOException ex) 
+                {
+                    Logger.getLogger(GameWindow.class.getName()).log(Level.SEVERE, null, ex);
+                    actionButton.setEnabled(true);
+                }
+            }
+        }
+    }//GEN-LAST:event_actionButtonActionPerformed
+
+    private void cli_ser_ComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cli_ser_ComboBoxActionPerformed
+        if(cli_ser_ComboBox.getSelectedItem().equals("Server"))
+        {
+            ipTextField.setEnabled(false);
+            symbolTextField.setText("O");
+            actionButton.setText("Host");
+        }
+        else
+        {
+            ipTextField.setEnabled(true);
+            symbolTextField.setText("X");
+            actionButton.setText("Connect");            
+        }
+    }//GEN-LAST:event_cli_ser_ComboBoxActionPerformed
 
     /**
      * @param args the command line arguments
@@ -202,7 +379,7 @@ public class GameWindow extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(GameWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -212,6 +389,11 @@ public class GameWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton actionButton;
+    private javax.swing.JComboBox<String> cli_ser_ComboBox;
+    private javax.swing.JTextField ipTextField;
+    private javax.swing.JTextField nameTextField;
+    private javax.swing.JTextField symbolTextField;
     private javax.swing.JLabel title;
     // End of variables declaration//GEN-END:variables
 }
